a1fe1f6b8aba126869e98b43c17aadd6c4ae0716
Eric Lee <macbookpro4eric@Erics-MacBook-Pro.local>
2020-03-12 21:52:47 +0900

    add getGoogleAppReivew()

--- AppReviewClassifier/AppReviewManager.py
+++ AppReviewClassifier/AppReviewManager.py
@@ -165,6 +165,118 @@ class AppReviewManager:
         return summeryTableList
 
 
+    def getGoogleReview(self,urls, browserCheckoutCount=None):
+
+        directory = "appReview"
+
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+        if sys.platform == "linux":
+            driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")  # Linux
+        elif sys.platform == "darwin":
+            driver = webdriver.Chrome("/usr/local/bin/chromedriver")  # for MACOS
+        else:
+            driver = webdriver.Chrome("C:\\Users\\Eric\\Documents\\Python\\chromedriver")  # for Windows
+
+        WebDriverWait(driver, 5)
+
+        summeryTableList = []
+
+        for url in urls:
+            authors = []
+            reviewDates = []
+            ratings = []
+            bodies = []
+
+            driver.get(url)
+
+            page = driver.page_source
+
+            soup_expatistan = BeautifulSoup(page, "html.parser")
+
+            appName = soup_expatistan.find("h1", class_="AHFaub").span.text
+            print("App name: ", appName)
+
+            ratingCount = str(soup_expatistan.find("span", class_="AYi5wd TBRnV").findAll("span")[0].text).replace(",",
+                                                                                                                   "")
+            print("Rate Count: " + ratingCount)
+            if int(ratingCount) > 10000:
+                ratingCount = "10000"
+
+            # open all reviews
+            url = url + '&showAllReviews=true'
+            driver.get(url)
+
+            time.sleep(5)  # wait dom ready
+            bufferBody = ""
+            browserCheckCount = 0
+            for i in range(1, round(int(ratingCount) / 10)):
+                print("progress {i}".format(i=i))
+                driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')  # scroll to load other reviews
+                page = driver.page_source
+                soup_expatistan = BeautifulSoup(page, "html.parser")
+                if bufferBody == str(
+                        soup_expatistan.findAll("div", class_="d15Mdf")[-1].find("div", class_="UD7Dzf").text):
+                    browserCheckCount += 1
+                    print("Scroll down fail, no content loaded")
+                else:
+                    bufferBody = str(
+                        soup_expatistan.findAll("div", class_="d15Mdf")[-1].find("div", class_="UD7Dzf").text)
+                soup_loadMore = soup_expatistan.findAll("div", class_="PFAhAf")
+
+                try:
+                    if browserCheckCount > browserCheckoutCount:
+                        break
+                except:
+                    pass
+
+                if soup_loadMore:
+                    driver.execute_script('document.querySelector("div.PFAhAf").querySelector("span").click();')
+                time.sleep(1)
+
+            page = driver.page_source
+            soup_expatistan = BeautifulSoup(page, "html.parser")
+            expand_pages = soup_expatistan.findAll("div", class_="d15Mdf")
+            counter = 1
+            for expand_page in expand_pages:
+                try:
+
+                    authorName = str(expand_page.find("span", class_="X43Kjb").text)
+                    authors.append(authorName)
+                    reviewDate = expand_page.find("span", class_="p2TkOb").text
+                    reviewDates.append(reviewDate)
+
+                    reviewer_ratings = expand_page.find("div", class_="pf5lIe").find_next()['aria-label'];
+                    reviewer_ratings = reviewer_ratings.split('(')[0]
+                    reviewer_ratings = ''.join(x for x in reviewer_ratings if x.isdigit())
+                    ratings.append(reviewer_ratings)
+                    reviewBody = str(expand_page.find("div", class_="UD7Dzf").text)
+                    bodies.append(reviewBody)
+                    counter += 1
+                except:
+                    pass
+
+            reviewLib = {
+                "Author Name": authors,
+                "Review Date": reviewDates,
+                "Reviewer Ratings": ratings,
+                "Review Body": bodies
+            }
+            reviewTable = pd.DataFrame(reviewLib)
+
+            fileName = 'GoogleReviewTable-{appName}.xlsx'.format(appName=appName, ratingCount=ratingCount)
+            fileNameCsv = 'GoogleReviewTable-{appName}.csv'.format(appName=appName, ratingCount=ratingCount)
+
+            reviewTable.to_excel(os.path.join(directory, fileName), sheet_name="Sheet1")
+            print(reviewTable)
+
+            summeryTableList.append(reviewTable)
+        driver.quit()
+        return summeryTableList
+
+
+
 
 
 
